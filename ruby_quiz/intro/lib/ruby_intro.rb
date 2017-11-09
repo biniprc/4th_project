4c152efffb3b514867c18a526a9db554f68b199a
biniprc <biniprc@hotmail.com>
2017-11-09 11:57:23 +0900

    modify comment

--- ruby_quiz/intro/lib/ruby_intro.rb
+++ ruby_quiz/intro/lib/ruby_intro.rb
@@ -1,28 +1,29 @@
 # Part 1
 
-# arr에 있는 모든 요소를 더하세요.
+# arr에 있는 모든 요소를 더한 값을 return하세요.
 def sum arr
   # YOUR CODE HERE
 end
 
-# arr에 있는 최대값 2개를 더하세요.
+# arr에 있는 가장 큰 두 수를 더해서 return하세요.
+# 가장 큰 수가 동일한 경우 두 가지를 더하세요.
 def max_2_sum arr
   # YOUR CODE HERE
 end
 
-# n번째 값까지 더하세요.
+# arr안에 있는 두 수를 더하여, n이 될 수 있는지의 여부를 return하세요.
 def sum_to_n? arr, n
   # YOUR CODE HERE
 end
 
 # Part 2
 
-# "hello 이름"을 출력하는 코드를 쓰세요.
+# "Hello 이름"(H 대문자 주의)을 return하세요.
 def hello(name)
   # YOUR CODE HERE
 end
 
-# 자음으로 시작하는지 판별하는 코드를 쓰세요.
+# 임의의 문자열 s가 자음으로 시작하는지의 여부를 return 하세요.
 def starts_with_consonant? s
   # YOUR CODE HERE
 end
